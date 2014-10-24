eaa674986c9e24f4d2bd36508350eaaad0d8ce8b
Hajtosek <hajtosek@gmail.com>
2014-10-24 20:42:15 +0200

    Bang

---
+++ dodawanie.php
@@ -0,0 +1,18 @@
+﻿<?php
+echo('
+<FORM method="GET" action="dodawanie.php">
+Podaj pierwszą liczbę: <INPUT type="text" name="liczba1"><BR>
+Podaj drugą liczbę: <INPUT type="text" name="liczba2"><BR>
+<INPUT type="submit" value="Dodaj">
+</FORM>
+');
+if(isset($_GET['liczba1'])&&isset($_GET['liczba2']))
+{
+	$liczba1=$_GET['liczba1'];
+	$liczba2=$_GET['liczba2'];
+	if(is_numeric($liczba1)&&(is_numeric($liczba2))) 
+		echo("$liczba1+$liczba2=".($liczba1+$liczba2));
+	else
+		echo('Musisz wprowadzić dane');
+}
+?>
---
+++ pesel.php
@@ -0,0 +1,27 @@
+﻿<?php
+	echo('
+		<FORM method="GET">
+	
+		Podaj PESEL: <INPUT type="text" name="pesel"><br>
+		<INPUT type="submit" value="Spawdz">
+		</FORM>
+		');
+		if(isset($_GET['pesel']))
+		{
+		  $pesel=$_GET['pesel'];
+		  $suma=(substr($pesel,0,1))*1+(substr($pesel,1,1))*3+(substr($pesel,2,1))*7+(substr($pesel,3,1))*9+
+		  (substr($pesel,4,1))*1+(substr($pesel,5,1))*3+(substr($pesel,6,1))*7+(substr($pesel,7,1))*9
+		  +(substr($pesel,8,1))*1+(substr($pesel,9,1))*3+(substr($pesel,10,1)); 
+		  
+		  
+		  if($suma%10==0)
+		  
+		echo('Poprawny');
+		
+	else
+		
+		echo('Zly');
+		
+		}
+		
+?>
